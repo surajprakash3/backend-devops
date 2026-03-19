0ef9cc48241d81a21102b1d494cd5361550341fb
Suraj Prakash <surajprak201@gmail.com>
2026-03-19 21:47:22 +0530

    hellow worl

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-res.send('Hello World!');
+res.send('Hello Wormbkjld!');
 });
 app.post('/data', (req, res) => {
 const data = req.body;
